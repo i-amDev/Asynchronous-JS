cab9a0c1530713ca5b9fc299e2b695745433d16c
i-amDev <dev79037@gmail.com>
2026-03-18 11:43:06 +0530

    - Created a readFilePromise and a writeFilePromise from scratch
    - Used both the promises to refactor the code completely
    
    - Used then blocks and catch block for resolve and reject part

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-const fs = require('fs');
-const superagent = require('superagent');
+const fs = require("fs");
+const superagent = require("superagent");
 
-const readFilePromise = file => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(file, (error, data) => {
-            if (error) reject('Could not find that file 💥');
-            resolve(data);
-        });
+const readFilePromise = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (error, data) => {
+      if (error) reject("Could not find that file 💥");
+      resolve(data);
     });
+  });
 };
 
-readFilePromise(`${__dirname}/dog.txt`).then(data => {
-    console.log(`Breed: ${data}`);
-
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).then(response => {
-        console.log(response.body.message);
-
-        fs.writeFile('dog-img.txt', response.body.message, error => {
-            if (error) return console.log(error.message);
-            console.log('Random dog image saved to file!');
-        });
-    }).catch(error => {
-        console.log(error.message);
+const writeFilePromise = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (error) => {
+      if (error) reject("Could not write file ❌");
+      resolve("Success ✅");
     });
-});
+  });
+};
+
+readFilePromise(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((response) => {
+    console.log(response.body.message);
+    return writeFilePromise("dog-img.txt", response.body.message);
+  })
+  .then(() => {
+    console.log("Random dog🐶 image saved to file!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
